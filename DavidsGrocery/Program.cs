206454c15dd09f92389ab922ca3c77769ef196a2
David Rector <rector@rdacorp.com>
2022-06-07 15:42:41 -0400

    app insights

--- DavidsGrocery/Program.cs
+++ DavidsGrocery/Program.cs
@@ -56,6 +56,7 @@ builder.Services.AddTransient<IInventoryRepository, InventoryRepository>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]);
 
 var app = builder.Build();
 
